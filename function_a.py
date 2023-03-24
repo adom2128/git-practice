fb2671c39265992182ce3b4577bc48ca1f31a090
Raissa Barbosa <raissaglaucie@icloud.com>
2023-03-24 17:12:24 -0400

    function is done

--- function_a.py
+++ function_a.py
@@ -7,3 +7,32 @@ def most_common_value(number_list):
 if __name__ == "__main__":
     nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
     print(f"Most common value = {most_common_value(nums)}")
+
+
+def silly_sum():
+    """ reads numbers from the user
+        summing as we go until either
+        the user enters 0, or
+        the sum reaches or exceeds 1000
+    """
+    num = int(input('Please enter a number => '))
+    sum = 0
+
+    while num != 0:
+        sum += num
+        if sum >= 1000:
+            break
+
+        num = int(input('Please enter a number => '))
+
+    return sum
+```
+**function_c.py** - for 3-person teams
+
+```python
+def merge_lists(list_a, list_b):
+    """ Returns a new list which is
+        a combination of list_a and list_b
+        without any duplicate elements.
+    """
+    return list(set(list_a) | set(list_b))
